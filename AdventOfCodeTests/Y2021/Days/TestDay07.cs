6335611f0fa6f06bdce42d812a238ee83e15d24d
EnErG23 <vandenabbeelegeert@gmail.com>
2021-12-07 11:34:59 +0100

    Added tests 2021.7

---
+++ AdventOfCodeTests/Y2021/Days/TestDay07.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AdventOfCode.Y2021.Days;
+
+namespace AdventOfCodeTests.Y2021.Days
+{
+    [TestClass]
+    public class TestDay07
+    {
+        Day07? day;
+        string[] results = new[] { "37", "168", "355764", "99634572" };
+
+        [TestMethod]
+        public void Test_Run_Part1()
+        {
+            day = new(true);
+
+            string result = day.RunPart1();
+
+            Assert.AreEqual(results[0], result);
+        }
+
+        [TestMethod]
+        public void Test_Run_Part2()
+        {
+            day = new(true);
+
+            string result = day.RunPart2();
+
+            Assert.AreEqual(results[1], result);
+        }
+
+        [TestMethod]
+        public void Run_Part1()
+        {
+            day = new(false);
+
+            string result = day.RunPart1();
+
+            Assert.AreEqual(results[2], result);
+        }
+
+        [TestMethod]
+        public void Run_Part2()
+        {
+            day = new(false);
+
+            string result = day.RunPart2();
+
+            Assert.AreEqual(results[3], result);
+        }
+    }
+}
