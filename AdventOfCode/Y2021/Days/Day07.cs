0fefd9c704108b6ce0b34420ee92c82e3a8a6411
EnErG23 <vandenabbeelegeert@gmail.com>
2021-12-07 11:11:20 +0100

    Solved 2021.7

--- AdventOfCode/Y2021/Days/Day07.cs
+++ AdventOfCode/Y2021/Days/Day07.cs
@@ -12,6 +12,18 @@ namespace AdventOfCode.Y2021.Days
         {
             long result = 0;
 
+            var crabs = Inputs[0].Split(",").Select(i => int.Parse(i)).ToList();
+
+            List<int> fuel = new List<int>(new int[crabs.Max()]);
+
+            for (int f = 0; f < fuel.Count; f++)
+            {
+                for (int c = 0; c < crabs.Count; c++)
+                    fuel[f] += Math.Abs(f - crabs[c]);
+            }
+
+            result = fuel.Min();
+
             return result.ToString();
         }
 
@@ -19,7 +31,36 @@ namespace AdventOfCode.Y2021.Days
         {
             long result = 0;
 
+            var crabs = Inputs[0].Split(",").Select(i => int.Parse(i)).ToList();
+
+            List<long> fuel = new List<long>(new long[crabs.Max()]);
+
+            for (int f = 0; f < fuel.Count; f++)
+                for (int c = 0; c < crabs.Count; c++)
+                    if (f != crabs[c])
+                    {
+                        fuel[f] += GetFuel(crabs[c], f);
+                        if (f == 5)
+                            Console.WriteLine($"From {crabs[c]} to {f} costs {GetFuel(crabs[c], f)} fuel");
+                    }
+
+            Console.WriteLine(String.Join(",", fuel));
+
+            result = fuel.Min();
+
             return result.ToString();
+        }
+
+        private long GetFuel(int from, int to)
+        {
+            long result = 0;
+
+            for (int i = 1; i <= Math.Abs(from - to); i++)
+                result += i;
+
+            //Console.WriteLine($"From {from} to {to} costs {result} fuel");
+
+            return result;
         }
     }
 }
