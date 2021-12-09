ffee51ecdf9f7beb3a77ff170afd9571a1995aae
Geert Van den Abbeele <geert@semko.be>
2021-12-09 02:08:39 +0100

    Prepared 2021.9

---
+++ AdventOfCode/Y2021/Days/Day09.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Models;
+
+namespace AdventOfCode.Y2021.Days
+{
+    public class Day09 : Day
+    {
+        public Day09(int year, int day, bool test) : base(year, day, test) { }
+
+        public override string RunPart1()
+        {
+            return "Undefined";
+        }
+
+        public override string RunPart2()
+        {
+            return "Undefined";
+        }
+    }
+}
