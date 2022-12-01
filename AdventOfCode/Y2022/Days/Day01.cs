b16dcd1efca243847309501b410a912fa541722e
Geert Van den Abbeele <geert@semko.be>
2022-12-01 10:55:03 +0100

    Solved 2022.1

---
+++ AdventOfCode/Y2022/Days/Day01.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode.Models;
+
+namespace AdventOfCode.Y2022.Days
+{
+    public class Day01 : Day
+    {
+        List<int> ElveCalories = new List<int>();
+
+        public Day01(int year, int day, bool test) : base(year, day, test) { }
+
+        public override string RunPart1()
+        {
+            int calories = 0;
+
+            foreach (var input in Inputs)
+            {
+                if (input == "")
+                {
+                    ElveCalories.Add(calories);
+                    calories = 0;
+                }
+                else
+                {
+                    calories += int.Parse(input);
+                }
+            }
+
+            return ElveCalories.Max(e => e).ToString();
+        }
+
+        public override string RunPart2()
+        {
+            return ElveCalories.OrderByDescending(e => e).Take(3).Sum(e => e).ToString();
+        }
+    }
+}
---
+++ AdventOfCode/Y2022/Days/Day02.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Models;
+
+namespace AdventOfCode.Y2022.Days
+{
+    public class Day02 : Day
+    {
+        public Day02(int year, int day, bool test) : base(year, day, test) { }
+
+        public override string RunPart1()
+        {
+            return "Undefined";
+        }
+
+        public override string RunPart2()
+        {
+            return "Undefined";
+        }
+    }
+}
