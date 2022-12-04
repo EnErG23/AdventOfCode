c4b9e2c8cdcccca6681d45331ed351c8f6b305be
EnErG23 <vandenabbeelegeert@gmail.com>
2022-12-04 22:42:55 +0100

    Cleaned 2022.1

--- AdventOfCode/Y2022/Days/Day01.cs
+++ AdventOfCode/Y2022/Days/Day01.cs
@@ -4,57 +4,32 @@ namespace AdventOfCode.Y2022.Days
 {
     public class Day01 : Day
     {
-        private List<int>? elfCalories;
-
         public Day01(int year, int day, bool test) : base(year, day, test) { }
 
         public override string RunPart1()
         {
-            elfCalories = new List<int>();
-
-            int calories = 0;
-
-            foreach (var input in Inputs)
-            {
-                if (input == "")
-                {
-                    elfCalories.Add(calories);
-                    calories = 0;
-                }
-                else
-                {
-                    calories += int.Parse(input);
-                }
-            }
-            elfCalories.Add(calories);
-
-            return elfCalories.Max().ToString();
+            return String.Join(';', Inputs)
+                .Split(";;")
+                .ToList()
+                .Select(e => e.Split(';')
+                    .Select(s => int.Parse(s))
+                    .Sum())
+                .Max()
+                .ToString();
         }
 
         public override string RunPart2()
         {
-            if (elfCalories == null)
-            {
-                elfCalories = new List<int>();
-
-                int calories = 0;
-
-                foreach (var input in Inputs)
-                {
-                    if (input == "")
-                    {
-                        elfCalories.Add(calories);
-                        calories = 0;
-                    }
-                    else
-                    {
-                        calories += int.Parse(input);
-                    }
-                }
-                elfCalories.Add(calories);
-            }
-
-            return elfCalories.OrderByDescending(e => e).Take(3).Sum().ToString();
+            return String.Join(';', Inputs)
+                .Split(";;")
+                .ToList()
+                .Select(e => e.Split(';')
+                    .Select(s => int.Parse(s))
+                    .Sum())
+                .OrderByDescending(e => e)
+                .Take(3)
+                .Sum()
+                .ToString();
         }
     }
 }
