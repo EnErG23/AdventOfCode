93b205c55ed46f69b3864686d7e1ddfd68cccfdb
Geert Van den Abbeele <geert@semko.be>
2022-12-05 12:28:28 +0100

    Solved 2022.5

--- AdventOfCode/Y2022/Days/Day05.cs
+++ AdventOfCode/Y2022/Days/Day05.cs
@@ -4,16 +4,67 @@ namespace AdventOfCode.Y2022.Days
 {
     public class Day05 : Day
     {
-        public Day05(int year, int day, bool test) : base(year, day, test) { }
+        private readonly List<string> _stacks;
+        private readonly List<List<int>> _steps;
+
+        public Day05(int year, int day, bool test) : base(year, day, test)
+        {
+            //Parse stacks
+            _stacks = new List<string>();
+
+            for (int i = 0; i < int.Parse(Inputs.FirstOrDefault(i => i[1] == '1').Replace(" ", "").Last().ToString()); i++)
+                _stacks.Add("");
+
+            foreach (var input in Inputs)
+            {
+                if (input[1] == '1')
+                    break;
+
+                var row = input.Replace("    ", "[ ] ").Replace("][ ] ", "] [ ]").Replace("] [", "").Replace("[", "").Replace("]", "");
+
+                int stack = 0;
+                foreach (var package in row)
+                    _stacks[stack++] += $"{package}";
+            }
+
+            _stacks = _stacks
+                .Select(s => s.Replace(" ", ""))
+                .ToList();
+
+            //Parse steps
+            _steps = Inputs
+                .Where(i => i.Contains("move"))
+                .Select(i => i.Replace("move ", "").Replace("from ", "").Replace("to ", "").Split(" ").Select(s => int.Parse(s)).ToList())
+                .ToList();
+        }
 
         public override string RunPart1()
         {
-            return "undefined";
+            foreach (var step in _steps)
+            {
+                int length = _stacks[step[1] - 1].Length;
+
+                for (int i = 0; i < Math.Min(step[0], length); i++)
+                {
+                    _stacks[step[2] - 1] = _stacks[step[1] - 1][0] + _stacks[step[2] - 1];
+                    _stacks[step[1] - 1] = _stacks[step[1] - 1].Substring(1);
+                }
+            }
+
+            return String.Join("", _stacks.Select(s => s[0]));
         }
 
         public override string RunPart2()
         {
-            return "undefined";
+            foreach (var step in _steps)
+            {
+                int toMove = Math.Min(step[0],_stacks[step[1] - 1].Length);
+
+                _stacks[step[2] - 1] = _stacks[step[1] - 1].Substring(0,toMove) + _stacks[step[2] - 1];
+                _stacks[step[1] - 1] = _stacks[step[1] - 1].Substring(toMove);
+            }
+
+            return String.Join("", _stacks.Select(s => s[0]));
         }
     }
 }
