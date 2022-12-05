5585b86ec738417599b819795922d4a0fba92162
EnErG23 <vandenabbeelegeert@gmail.com>
2022-12-05 02:13:36 +0100

    Cleaned 2022.2

--- AdventOfCode/Y2022/Days/Day02.cs
+++ AdventOfCode/Y2022/Days/Day02.cs
@@ -8,118 +8,23 @@ namespace AdventOfCode.Y2022.Days
 
         public override string RunPart1()
         {
-            int score = 0;
-
-            foreach (var input in Inputs)
-            {
-                var game = input.Replace("A", "R").Replace("B", "P").Replace("C", "S").Replace("X", "R").Replace("Y", "P").Replace("Z", "S");
-
-                switch(game.Split(" ")[0])
-                {
-                    case "R":
-                        switch (game.Split(" ")[1])
-                        {
-                            case "R":
-                                score += 4;
-                                break;
-                            case "P":
-                                score += 8;
-                                break;
-                            case "S":
-                                score += 3;
-                                break;
-                        }
-                        break;
-                    case "P":
-                        switch (game.Split(" ")[1])
-                        {
-                            case "R":
-                                score += 1;
-                                break;
-                            case "P":
-                                score += 5;
-                                break;
-                            case "S":
-                                score += 9;
-                                break;
-                        }
-                        break;
-                    case "S":
-                        switch (game.Split(" ")[1])
-                        {
-                            case "R":
-                                score += 7;
-                                break;
-                            case "P":
-                                score += 2;
-                                break;
-                            case "S":
-                                score += 6;
-                                break;
-                        }
-                        break;
-                }
-            }
-
-            return score.ToString();
+            return Inputs.Select(i => (char.Parse(i.Split(' ')[0]) - 64, char.Parse(i.Split(' ')[1]) - 87))
+                .Sum(i => i.Item2 + (i.Item1 == i.Item2 ? 3 :
+                                    (i.Item1 == 1 ? (i.Item2 == 2 ? 6 : 0) :
+                                    (i.Item1 == 2 ? (i.Item2 == 1 ? 0 : 6) :
+                                    (i.Item2 == 1 ? 6 : 0)))))
+                .ToString();
         }
 
         public override string RunPart2()
         {
-            int score = 0;
-
-            foreach (var input in Inputs)
-            {
-                var game = input.Replace("A", "R").Replace("B", "P").Replace("C", "S");
-
-                switch (game.Split(" ")[0])
-                {
-                    case "R":
-                        switch (game.Split(" ")[1])
-                        {
-                            case "X":
-                                score += 3;
-                                break;
-                            case "Y":
-                                score += 4;
-                                break;
-                            case "Z":
-                                score += 8;
-                                break;
-                        }
-                        break;
-                    case "P":
-                        switch (game.Split(" ")[1])
-                        {
-                            case "X":
-                                score += 1;
-                                break;
-                            case "Y":
-                                score += 5;
-                                break;
-                            case "Z":
-                                score += 9;
-                                break;
-                        }
-                        break;
-                    case "S":
-                        switch (game.Split(" ")[1])
-                        {
-                            case "X":
-                                score += 2;
-                                break;
-                            case "Y":
-                                score += 6;
-                                break;
-                            case "Z":
-                                score += 7;
-                                break;
-                        }
-                        break;
-                }
-            }
-
-            return score.ToString();
+            return Inputs.Select(i => (char.Parse(i.Split(' ')[0]) - 64, char.Parse(i.Split(' ')[1]) - 87))
+                .Sum(i => (i.Item2 == 2 ? (i.Item1 + 3) :
+                          (i.Item2 == 1 ? (i.Item1 == 1 ? 3 : 
+                                          (i.Item1 == 2 ? 1 : 2)) :
+                          (i.Item1 == 1 ? 8 :
+                          (i.Item1 == 2 ? 9 : 7)))))
+                .ToString();
         }
     }
 }
