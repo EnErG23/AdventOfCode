05d29b4ef20798650993743e006889d87ea76e0d
Geert Van den Abbeele <geert@semko.be>
2022-12-06 09:59:17 +0100

    Updated test 2022.06

--- AdventOfCodeTests/Y2022/Days/TestDay06.cs
+++ AdventOfCodeTests/Y2022/Days/TestDay06.cs
@@ -12,7 +12,7 @@ namespace AdventOfCodeTests.Y2022.Days
 
         Day06? testDay;
         Day06? day;
-        string[] results = new[] { "undefined", "undefined", "undefined", "undefined" };
+        string[] results = new[] { "7", "1210", "19", "3476" };
 
         public TestDay06()
         {
