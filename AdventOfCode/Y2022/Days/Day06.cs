6a022fd2a0c4eb29206275bdd9453af572c361f5
Geert Van den Abbeele <geert@semko.be>
2022-12-06 10:11:51 +0100

    Cleaned 2022.06

--- AdventOfCode/Y2022/Days/Day06.cs
+++ AdventOfCode/Y2022/Days/Day06.cs
@@ -4,44 +4,33 @@ namespace AdventOfCode.Y2022.Days
 {
     public class Day06 : Day
     {
-        public Day06(int year, int day, bool test) : base(year, day, test) { }
+        private readonly string _datastream;
+        private readonly int _distChars1;
+        private readonly int _distChars2;
+
+        public Day06(int year, int day, bool test) : base(year, day, test)
+        {
+            _datastream = Inputs.First();
+            _distChars1 = 4;
+            _distChars2 = 14;
+        }
 
         public override string RunPart1()
         {
-            int result = 0;
-            var datastream = Inputs.First();
-
-            for (int i = 0; i < datastream.Length - 4; i++)
-            {
-                var possibleMarker = datastream.Substring(i, 4);
-
-                if (possibleMarker.Distinct().Count() == possibleMarker.Length)
-                {
-                    result = i + 4;
-                    break;
-                }
-            }
-
-            return result.ToString();
+            return Enumerable
+                .Range(_distChars1, _datastream.Length)
+                .ToList()
+                .FirstOrDefault(m => _datastream.Substring(m - _distChars1, _distChars1).Distinct().Count() == _distChars1)
+                .ToString();
         }
 
         public override string RunPart2()
         {
-            int result = 0;
-            var datastream = Inputs.First();
-
-            for (int i = 0; i < datastream.Length - 14; i++)
-            {
-                var possibleMarker = datastream.Substring(i, 14);
-
-                if (possibleMarker.Distinct().Count() == possibleMarker.Length)
-                {
-                    result = i + 14;
-                    break;
-                }
-            }
-
-            return result.ToString();
+            return Enumerable
+                .Range(_distChars2, _datastream.Length)
+                .ToList()
+                .FirstOrDefault(m => _datastream.Substring(m - _distChars2, _distChars2).Distinct().Count() == _distChars2)
+                .ToString();
         }
     }
 }
