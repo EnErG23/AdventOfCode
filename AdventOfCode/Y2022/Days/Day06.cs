48094250aac58294919d66a149433d4b9f2fc889
Geert Van den Abbeele <geert@semko.be>
2022-12-06 09:57:43 +0100

    Solved 2022.06

--- AdventOfCode/Y2022/Days/Day06.cs
+++ AdventOfCode/Y2022/Days/Day06.cs
@@ -8,12 +8,40 @@ namespace AdventOfCode.Y2022.Days
 
         public override string RunPart1()
         {
-            return "undefined";
+            int result = 0;
+            var datastream = Inputs.First();
+
+            for (int i = 0; i < datastream.Length - 4; i++)
+            {
+                var possibleMarker = datastream.Substring(i, 4);
+
+                if (possibleMarker.Distinct().Count() == possibleMarker.Length)
+                {
+                    result = i + 4;
+                    break;
+                }
+            }
+
+            return result.ToString();
         }
 
         public override string RunPart2()
         {
-            return "undefined";
+            int result = 0;
+            var datastream = Inputs.First();
+
+            for (int i = 0; i < datastream.Length - 14; i++)
+            {
+                var possibleMarker = datastream.Substring(i, 14);
+
+                if (possibleMarker.Distinct().Count() == possibleMarker.Length)
+                {
+                    result = i + 14;
+                    break;
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
