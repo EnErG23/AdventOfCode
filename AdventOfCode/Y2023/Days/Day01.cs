d108fe2d44b34a5c8b702b3b0987c383e5178327
Geert Van den Abbeele <vandenabbeelegeert@gmail.com>
2023-12-01 06:29:32 +0100

    Solved 2023.01

--- AdventOfCode/Y2023/Days/Day01.cs
+++ AdventOfCode/Y2023/Days/Day01.cs
@@ -1,4 +1,6 @@
 ﻿using AdventOfCode.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
 
 namespace AdventOfCode.Y2023.Days
 {
@@ -8,12 +10,92 @@ namespace AdventOfCode.Y2023.Days
 
         public override string RunPart1()
         {
-            return "undefined";
+            var result = 0;
+
+            foreach (var input in Inputs)
+            {
+                char firstNumber = 'x';
+                char lastNumber = 'x';
+
+                foreach (var letter in input)
+                {
+                    if (Char.IsDigit(letter))
+                    {
+                        if (firstNumber == 'x')
+                        {
+                            firstNumber = letter;
+                        }
+                        else
+                        {
+                            lastNumber = letter;
+                        }
+                    }
+                }
+
+                if (lastNumber == 'x')
+                    lastNumber = firstNumber;
+
+                result += int.Parse($"{firstNumber}{lastNumber}");
+            }
+
+            return result.ToString();
         }
 
         public override string RunPart2()
         {
-            return "undefined";
+            var inputs = Test ? new List<string> { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" } : Inputs;
+            var numbers = new List<string> { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var result = 0;
+
+            foreach (var input in inputs)
+            {
+                char firstNumber = 'x';
+                char lastNumber = 'x';
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var letter = input[i];
+
+                    if (Char.IsDigit(letter))
+                    {
+                        if (firstNumber == 'x')
+                        {
+                            firstNumber = letter;
+                        }
+                        else
+                        {
+                            lastNumber = letter;
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < numbers.Count; j++)
+                        {
+                            var number = numbers[j];
+
+                            if (input.Substring(i, Math.Min(number.Length, input.Length - i)) == number)
+                            {
+                                if (firstNumber == 'x')
+                                {
+                                    firstNumber = char.Parse($"{j + 1}");
+                                }
+                                else
+                                {
+                                    lastNumber = char.Parse($"{j + 1}");
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (lastNumber == 'x')
+                    lastNumber = firstNumber;
+
+                result += int.Parse($"{firstNumber}{lastNumber}");
+            }
+
+            return result.ToString();
         }
     }
 }
--- AdventOfCodeTests/Y2023/Days/TestDay01.cs
+++ AdventOfCodeTests/Y2023/Days/TestDay01.cs
@@ -12,7 +12,7 @@ namespace AdventOfCodeTests.Y2023.Days
 
         Day01? testDay;
         Day01? day;
-        string[] results = new[] { "undefined", "undefined", "undefined", "undefined" };
+        string[] results = new[] { "142", "undefined", "undefined", "undefined" };
 
         public TestDay01()
         {
