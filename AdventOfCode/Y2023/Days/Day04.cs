ced02448dae39cf79678ffe02df0d762f6e748cc
Geert Van den Abbeele <vandenabbeelegeert@gmail.com>
2023-12-04 11:51:28 +0100

    Cleaned 2023.04

--- AdventOfCode/Y2023/Days/Day04.cs
+++ AdventOfCode/Y2023/Days/Day04.cs
@@ -4,82 +4,50 @@ namespace AdventOfCode.Y2023.Days
 {
     public class Day04 : Day
     {
-        public Day04(int year, int day, bool test) : base(year, day, test) { }
-
-        public override string RunPart1()
+        private List<Game> _games;
+        private double _totalScore => _games.Sum(g => g.Score);
+        private int _totalCards
         {
-            double score = 0;
+            get
+            {
+                _games.ForEach(game => _games.Where(g => g.ID > game.ID && g.ID <= game.ID + game.Matches).ToList().ForEach(g => g.Cards += game.Matches > 0 ? game.Cards : 0));
+
+                return _games.Sum(g => g.Cards);
+            }
+        }
+
+        public Day04(int year, int day, bool test) : base(year, day, test)
+        {
+            _games = new List<Game>();
 
             foreach (var game in Inputs)
             {
+                var id = int.Parse(game.Split(":")[0].Replace("   ", " ").Replace("  ", " ").Split(" ")[1]);
                 var winningNumbers = game.Split(":")[1].Split("|")[0].Trim().Replace("  ", " ").Split(" ").Select(n => int.Parse(n)).ToList();
                 var gameNumbers = game.Split(":")[1].Split("|")[1].Trim().Replace("  ", " ").Split(" ").Select(n => int.Parse(n)).ToList();
 
-                if ((gameNumbers.Count(g => winningNumbers.Contains(g)) > 0))
-                    score += Math.Pow(2, Convert.ToDouble(gameNumbers.Count(g => winningNumbers.Contains(g)) - 1));
+                _games.Add(new Game(id, winningNumbers, gameNumbers));
             }
-
-            return score.ToString();
         }
 
-        public override string RunPart2()
-        {
-            var games = new List<Game> { };
+        public override string RunPart1() => _totalScore.ToString();
 
-            foreach (var game in Inputs)
-            {
-                var id = int.Parse(game.Split(":")[0].Replace("   "," ").Replace("  ", " ").Split(" ")[1]);
-                var winningNumbers = game.Split(":")[1].Split("|")[0].Trim().Replace("  ", " ").Split(" ").Select(n => int.Parse(n)).ToList();
-                var gameNumbers = game.Split(":")[1].Split("|")[1].Trim().Replace("  ", " ").Split(" ").Select(n => int.Parse(n)).ToList();
-
-                games.Add(new Game(id, winningNumbers, gameNumbers));
-            }
-
-            int maxID = games.Max(g => g.ID);
-
-            for (int i = 1; i <= maxID; i++)
-            {
-                var game = games.First(g => g.ID == i);
-
-                if (game.Matches > 0)
-                {
-                    var cards = games.Count(g => g.ID == i);
-
-                    for (int c = 0; c < cards; c++)
-                        for (int j = 1; j <= game.Matches; j++)
-                            if (game.ID + j <= maxID)
-                                games.Add(games.First(g => g.ID == game.ID + j));
-                }
-            }
-
-            return games.Count().ToString();
-        }
+        public override string RunPart2() => _totalCards.ToString();
     }
 
     public class Game
     {
         public int ID { get; set; }
+        public int Cards { get; set; }
         public List<int> WinningNumbers { get; set; }
         public List<int> Numbers { get; set; }
-        public int Matches
-        {
-            get
-            {
-                return Numbers.Count(n => WinningNumbers.Contains(n));
-            }
-
-        }
-        public double Score
-        {
-            get
-            {
-                return Matches > 0 ? Math.Pow(2, Matches - 1) : 0;
-            }
-        }
+        public int Matches => Numbers.Count(n => WinningNumbers.Contains(n));
+        public double Score => Matches > 0 ? Math.Pow(2, Convert.ToDouble(Numbers.Count(n => WinningNumbers.Contains(n)) - 1)) : 0;
 
         public Game(int iD, List<int> winningNumbers, List<int> numbers)
         {
             ID = iD;
+            Cards = 1;
             WinningNumbers = winningNumbers;
             Numbers = numbers;
         }
