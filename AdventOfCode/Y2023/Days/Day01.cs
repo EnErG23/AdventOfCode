798d7c1427c9c0667d1b8a68774355b7b1c4dbbd
Geert Van den Abbeele <vandenabbeelegeert@gmail.com>
2023-12-04 12:57:23 +0100

    Cleaned 2023.01

--- AdventOfCode/Y2023/Days/Day01.cs
+++ AdventOfCode/Y2023/Days/Day01.cs
@@ -19,32 +19,32 @@ namespace AdventOfCode.Y2023.Days
 
             foreach (var input in inputs)
             {
-                char firstNumber = 'x';
-                char lastNumber = 'x';
+                char firstDigit = 'x';
+                char lastDigit = 'x';
 
                 for (int i = 0; i < input.Length; i++)
                 {
-                    var letter = input[i];
-
-                    if (Char.IsDigit(letter))
-                        if (firstNumber == 'x')
-                            firstNumber = letter;
+                    if (firstDigit == 'x')
+                        if (char.IsDigit(input[i]))
+                            firstDigit = input[i];
                         else
-                            lastNumber = letter;
-                    else
-                        for (int j = 0; j < numbers.Count; j++)
-                        {
-                            var number = numbers[j];
+                            for (int j = 1; j <= numbers.Count; j++)
+                                if (input.Substring(i, Math.Min(numbers[j - 1].Length, input.Length - i)) == numbers[j - 1])
+                                    firstDigit = char.Parse($"{j}");
 
-                            if (input.Substring(i, Math.Min(number.Length, input.Length - i)) == number)
-                                if (firstNumber == 'x')
-                                    firstNumber = char.Parse($"{j + 1}");
-                                else
-                                    lastNumber = char.Parse($"{j + 1}");
-                        }
+                    if (lastDigit == 'x')
+                        if (char.IsDigit(input[input.Length - i - 1]))
+                            lastDigit = input[input.Length - i - 1];
+                        else
+                            for (int j = 1; j <= numbers.Count; j++)
+                                if (input.Substring(input.Length - i - 1, Math.Min(numbers[j - 1].Length, input.Length - (input.Length - i) + 1)) == numbers[j - 1])
+                                    lastDigit = char.Parse($"{j}");
+
+                    if (firstDigit != 'x' && lastDigit != 'x')
+                        break;
                 }
 
-                result += int.Parse($"{firstNumber}{(lastNumber == 'x' ? firstNumber : lastNumber)}");
+                result += int.Parse($"{firstDigit}{lastDigit}");
             }
 
             return result.ToString();
