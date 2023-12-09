4fd1c3d88a89120fb205375d3648c80ad88bed44
Geert Van den Abbeele <vandenabbeelegeert@gmail.com>
2023-12-09 11:24:38 +0100

    Solved 2023.09

--- AdventOfCode/Y2023/Days/Day09.cs
+++ AdventOfCode/Y2023/Days/Day09.cs
@@ -4,16 +4,42 @@ namespace AdventOfCode.Y2023.Days
 {
     public class Day09 : Day
     {
-        public Day09(int year, int day, bool test) : base(year, day, test) { }
+        private List<List<List<int>>> _histories;
 
-        public override string RunPart1()
+        public Day09(int year, int day, bool test) : base(year, day, test)
         {
-            return "undefined";
+            _histories = new();
+            Inputs.ForEach(i => _histories.Add(new List<List<int>>() { i.Split(" ").Select(n => int.Parse(n)).ToList() }));
+
+            foreach (var history in _histories)
+            {
+                List<List<int>> sequences = new() { history.First() };
+
+                while (sequences.Last().Count(d => d != 0) > 0)
+                {
+                    var lastSequence = sequences.Last();
+
+                    List<int> newSequence = new List<int>();
+
+                    for (int i = 0; i < lastSequence.Count - 1; i++)
+                        newSequence.Add(lastSequence[i + 1] - lastSequence[i]);
+
+                    sequences.Add(newSequence);
+                }
+
+                sequences.Last().Insert(0, 0);
+                sequences.Last().Add(0);
+
+                for (int i = sequences.Count - 2; i >= 0; i--)
+                {
+                    sequences[i].Insert(0, sequences[i].First() - sequences[i + 1].First());
+                    sequences[i].Add(sequences[i].Last() + sequences[i + 1].Last());
+                }
+            }
         }
 
-        public override string RunPart2()
-        {
-            return "undefined";
-        }
+        public override string RunPart1() => _histories.Sum(h => h.First().Last()).ToString();
+
+        public override string RunPart2() => _histories.Sum(h => h.First().First()).ToString();
     }
 }
