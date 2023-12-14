af69e379dc87aecf8d49adcc626f673e4d8e6687
Geert Van den Abbeele <vandenabbeelegeert@gmail.com>
2023-12-14 10:38:25 +0100

    Solved 2023.14.01

--- AdventOfCode/Y2023/Days/Day14.cs
+++ AdventOfCode/Y2023/Days/Day14.cs
@@ -8,7 +8,32 @@ namespace AdventOfCode.Y2023.Days
 
         public override string RunPart1()
         {
-            return "undefined";
+            int totalLoad = 0;
+            List<(int, int)> stuckRocks = new();
+
+            for (int r = 0; r < Inputs.Count; r++)
+                for (int c = 0; c < Inputs[r].Length; c++)
+                    if (Inputs[r][c] == 'O')
+                    {
+                        int load = Inputs.Count - r;
+
+                        for (int rd = r - 1; rd >= -1; rd--)
+                        {
+                            if (rd == -1 || Inputs[rd][c] == '#' || stuckRocks.Contains((rd, c)))
+                            {
+                                stuckRocks.Add((rd + 1, c));
+                                break;
+                            }
+
+                            load++;
+                        }
+
+                        Console.WriteLine($"({r},{c}) => {load}");
+
+                        totalLoad += load;
+                    }
+
+            return totalLoad.ToString();
         }
 
         public override string RunPart2()
--- AdventOfCodeTests/Y2023/Days/TestDay14.cs
+++ AdventOfCodeTests/Y2023/Days/TestDay14.cs
@@ -12,7 +12,7 @@ namespace AdventOfCodeTests.Y2023.Days
 
         Day14? testDay;
         Day14? day;
-        string[] results = new[] { "undefined", "undefined", "undefined", "undefined" };
+        string[] results = new[] { "136", "108889", "64", "undefined" };
 
         public TestDay14()
         {
