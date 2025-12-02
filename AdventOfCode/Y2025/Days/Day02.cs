a7c5b1e6b8b41352e24d3bc414454ce5e4565b2a
Geert Van den Abbeele <vandenabbeelegeert@gmail.com>
2025-12-02 07:02:42 +0100

    Solved 2025.2

---
+++ AdventOfCode/Y2025/Days/Day02.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCode.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdventOfCode.Y2025.Days
+{
+    public class Day02 : Day
+    {
+        public Day02(int year, int day, bool test) : base(year, day, test)
+            => Inputs = Inputs[0].Trim().Split(",").ToList();
+
+        public override string RunPart1()
+        {
+            long result = 0;
+
+            foreach (var input in Inputs)
+            {
+                for (var id = long.Parse(input.Split("-")[0]); id <= long.Parse(input.Split("-")[1]); id++)
+                {
+                    var idString = id.ToString();
+
+                    if (idString.Length % 2 == 0)
+                    {
+                        var id1 = idString[..(idString.Length / 2)];
+                        var id2 = idString[(idString.Length / 2)..];
+
+                        if (id1 == id2)
+                            result += id;
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public override string RunPart2()
+        {
+            long result = 0;
+
+            foreach (var input in Inputs)
+            {
+                for (var id = long.Parse(input.Split("-")[0]); id <= long.Parse(input.Split("-")[1]); id++)
+                {
+                    var idString = id.ToString();
+
+                    for (int i = 1; i <= idString.Length / 2; i++)
+                    {
+                        var id1 = idString[..i];
+                        var counter = i;
+
+                        var isInvalid = true;
+
+                        while (counter < idString.Length)
+                        {
+                            try
+                            {
+                                if (idString.Substring(counter, i) != id1)
+                                {
+                                    isInvalid = false;
+                                    break;
+                                }
+                            }
+                            catch
+                            {
+                                isInvalid = false;
+                                break;
+                            }
+
+                            counter += i;
+                        }
+
+                        if (isInvalid)
+                        {                         
+                            result += id;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ AdventOfCodeTests/Y2025/Days/TestDay02.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AdventOfCode.Y2025.Days;
+
+namespace AdventOfCodeTests.Y2025.Days
+{
+    [TestClass]
+    public class TestDay02
+    {
+        int y;
+        int d;
+
+        Day02? testDay;
+        Day02? day;
+        string[] results = new[] { "1227775554", "23701357374", "4174379265", "34284458938" };
+
+        public TestDay02()
+        {
+            var type = GetType();
+            y = Convert.ToInt32(type.Namespace.Substring(type.Namespace.Length - 9, 4));
+            d = Convert.ToInt32(type.Name.Substring(7, 2));
+			testDay = new(y, d, true);
+			day = new(y,d,false);
+        }
+
+        [TestMethod]
+        public void A_Test_Run_Part1()
+        {
+            string result = testDay.RunPart1();
+
+            Assert.AreEqual(results[0], result);
+        }
+
+        [TestMethod]
+        public void B_Run_Part1()
+        {
+            string result = day.RunPart1();
+
+            Assert.AreEqual(results[1], result);
+        }
+
+        [TestMethod]
+        public void C_Test_Run_Part2()
+        {
+            string result = testDay.RunPart2();
+
+            Assert.AreEqual(results[2], result);
+        }
+
+        [TestMethod]
+        public void D_Run_Part2()
+        {
+            string result = day.RunPart2();
+
+            Assert.AreEqual(results[3], result);
+        }
+    }
+}
